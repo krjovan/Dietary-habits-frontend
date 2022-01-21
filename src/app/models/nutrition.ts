417b6adc5913ddb3a48f3b31f1bb064b71b2e5c3
krjovan <jovankron@gmail.com>
2022-01-21 16:35:11 +0100

    Added CRUD for Nutrition collection

---
+++ src/app/models/nutrition.ts
@@ -0,0 +1,149 @@
+export class Nutrition {
+  _id?: String;
+  name: String;
+  calories: Number;
+  total_fat_g
+    : Number;
+  saturated_fat_g
+    : Number;
+  cholesterol_mg
+    : Number;
+  sodium_mg
+    : Number;
+  choline_mg
+    : Number;
+  folate_mcg
+    : Number;
+  folic_acid_mcg
+    : Number;
+  niacin_mg
+    : Number;
+  pantothenic_acid_mg
+    : Number;
+  riboflavin_mg
+    : Number;
+  thiamin_mg
+    : Number;
+  vitamin_a_IU
+    : Number;
+  vitamin_a_rae_mcg
+    : Number;
+  carotene_alpha_mcg
+    : Number;
+  carotene_beta_mcg
+    : Number;
+  cryptoxanthin_beta_mcg
+    : Number;
+  lutein_zeaxanthin_mcg
+    : Number;
+  vitamin_b12_mcg
+    : Number;
+  vitamin_b6_mg
+    : Number;
+  vitamin_c_mg
+    : Number;
+  vitamin_d_IU
+    : Number;
+  vitamin_e_mg
+    : Number;
+  tocopherol_alpha_mg
+    : Number;
+  vitamin_k_mcg
+    : Number;
+  calcium_mg
+    : Number;
+  copper_mg
+    : Number;
+  irom_mg
+    : Number;
+  magnesium_mg
+    : Number;
+  manganese_mg
+    : Number;
+  phosphorous_mg
+    : Number;
+  potassium_mg
+    : Number;
+  selenium_mcg
+    : Number;
+  zink_mg
+    : Number;
+  protein_g
+    : Number;
+  alanine_g
+    : Number;
+  arginine_g
+    : Number;
+  aspartic_acid_g
+    : Number;
+  cystine_g
+    : Number;
+  glutamic_acid_g
+    : Number;
+  glycine_g
+    : Number;
+  histidine_g
+    : Number;
+  hydroxyproline_g
+    : Number;
+  isoleucine_g
+    : Number;
+  leucine_g
+    : Number;
+  lysine_g
+    : Number;
+  methionine_g
+    : Number;
+  phenylalanine_g
+    : Number;
+  proline_g
+    : Number;
+  serine_g
+    : Number;
+  threonine_g
+    : Number;
+  tryptophan_g
+    : Number;
+  tyrosine_g
+    : Number;
+  valine_g
+    : Number;
+  carbohydrate_g
+    : Number;
+  fiber_g
+    : Number;
+  sugars_g
+    : Number;
+  fructose_g
+    : Number;
+  galactose_g
+    : Number;
+  glucose_g
+    : Number;
+  lactose_g
+    : Number;
+  maltose_g
+    : Number;
+  sucrose_g
+    : Number;
+  fat_g
+    : Number;
+  saturated_fatty_acids_g
+    : Number;
+  monounsaturated_fatty_acids_g
+    : Number;
+  polyunsaturated_fatty_acids_g
+    : Number;
+  fatty_acids_total_trans_g
+    : Number;
+  alcohol_g
+    : Number;
+  ash_g
+    : Number;
+  caffeine_mg
+    : Number;
+  theobromine_mg
+    : Number;
+  water_g
+    : Number;
+}
