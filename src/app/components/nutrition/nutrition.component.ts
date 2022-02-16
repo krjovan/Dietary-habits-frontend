899539fcd14e71363e62e85dd61a1e91992f42ce
krjovan <jovankron@gmail.com>
2022-02-16 13:37:59 +0100

    Refactoring Nutrition TS

--- src/app/components/nutrition/nutrition.component.ts
+++ src/app/components/nutrition/nutrition.component.ts
@@ -10,159 +10,9 @@ import { NutritionService } from '../../services/nutrition.service';
 })
 export class NutritionComponent implements OnInit {
 
-  selectedNutrition: Nutrition = {
-    name: "",
-    calories: 0,
-    total_fat_g: 0,
-    saturated_fat_g: 0,
-    cholesterol_mg: 0,
-    sodium_mg: 0,
-    choline_mg: 0,
-    folate_mcg: 0,
-    folic_acid_mcg: 0,
-    niacin_mg: 0,
-    pantothenic_acid_mg: 0,
-    riboflavin_mg: 0,
-    thiamin_mg: 0,
-    vitamin_a_IU: 0,
-    vitamin_a_rae_mcg: 0,
-    carotene_alpha_mcg: 0,
-    carotene_beta_mcg: 0,
-    cryptoxanthin_beta_mcg: 0,
-    lutein_zeaxanthin_mcg: 0,
-    vitamin_b12_mcg: 0,
-    vitamin_b6_mg: 0,
-    vitamin_c_mg: 0,
-    vitamin_d_IU: 0,
-    vitamin_e_mg: 0,
-    tocopherol_alpha_mg: 0,
-    vitamin_k_mcg: 0,
-    calcium_mg: 0,
-    copper_mg: 0,
-    irom_mg: 0,
-    magnesium_mg: 0,
-    manganese_mg: 0,
-    phosphorous_mg: 0,
-    potassium_mg: 0,
-    selenium_mcg: 0,
-    zink_mg: 0,
-    protein_g: 0,
-    alanine_g: 0,
-    arginine_g: 0,
-    aspartic_acid_g: 0,
-    cystine_g: 0,
-    glutamic_acid_g: 0,
-    glycine_g: 0,
-    histidine_g: 0,
-    hydroxyproline_g: 0,
-    isoleucine_g: 0,
-    leucine_g: 0,
-    lysine_g: 0,
-    methionine_g: 0,
-    phenylalanine_g: 0,
-    proline_g: 0,
-    serine_g: 0,
-    threonine_g: 0,
-    tryptophan_g: 0,
-    tyrosine_g: 0,
-    valine_g: 0,
-    carbohydrate_g: 0,
-    fiber_g: 0,
-    sugars_g: 0,
-    fructose_g: 0,
-    galactose_g: 0,
-    glucose_g: 0,
-    lactose_g: 0,
-    maltose_g: 0,
-    sucrose_g: 0,
-    fat_g: 0,
-    saturated_fatty_acids_g: 0,
-    monounsaturated_fatty_acids_g: 0,
-    polyunsaturated_fatty_acids_g: 0,
-    fatty_acids_total_trans_g: 0,
-    alcohol_g: 0,
-    ash_g: 0,
-    caffeine_mg: 0,
-    theobromine_mg: 0,
-    water_g: 0
-  }
+  selectedNutrition: Nutrition = new Nutrition();
   nutritions: Nutrition [] = [];
-  nutrition: Nutrition = {
-    name: "",
-    calories: 0,
-    total_fat_g: 0,
-    saturated_fat_g: 0,
-    cholesterol_mg: 0,
-    sodium_mg: 0,
-    choline_mg: 0,
-    folate_mcg: 0,
-    folic_acid_mcg: 0,
-    niacin_mg: 0,
-    pantothenic_acid_mg: 0,
-    riboflavin_mg: 0,
-    thiamin_mg: 0,
-    vitamin_a_IU: 0,
-    vitamin_a_rae_mcg: 0,
-    carotene_alpha_mcg: 0,
-    carotene_beta_mcg: 0,
-    cryptoxanthin_beta_mcg: 0,
-    lutein_zeaxanthin_mcg: 0,
-    vitamin_b12_mcg: 0,
-    vitamin_b6_mg: 0,
-    vitamin_c_mg: 0,
-    vitamin_d_IU: 0,
-    vitamin_e_mg: 0,
-    tocopherol_alpha_mg: 0,
-    vitamin_k_mcg: 0,
-    calcium_mg: 0,
-    copper_mg: 0,
-    irom_mg: 0,
-    magnesium_mg: 0,
-    manganese_mg: 0,
-    phosphorous_mg: 0,
-    potassium_mg: 0,
-    selenium_mcg: 0,
-    zink_mg: 0,
-    protein_g: 0,
-    alanine_g: 0,
-    arginine_g: 0,
-    aspartic_acid_g: 0,
-    cystine_g: 0,
-    glutamic_acid_g: 0,
-    glycine_g: 0,
-    histidine_g: 0,
-    hydroxyproline_g: 0,
-    isoleucine_g: 0,
-    leucine_g: 0,
-    lysine_g: 0,
-    methionine_g: 0,
-    phenylalanine_g: 0,
-    proline_g: 0,
-    serine_g: 0,
-    threonine_g: 0,
-    tryptophan_g: 0,
-    tyrosine_g: 0,
-    valine_g: 0,
-    carbohydrate_g: 0,
-    fiber_g: 0,
-    sugars_g: 0,
-    fructose_g: 0,
-    galactose_g: 0,
-    glucose_g: 0,
-    lactose_g: 0,
-    maltose_g: 0,
-    sucrose_g: 0,
-    fat_g: 0,
-    saturated_fatty_acids_g: 0,
-    monounsaturated_fatty_acids_g: 0,
-    polyunsaturated_fatty_acids_g: 0,
-    fatty_acids_total_trans_g: 0,
-    alcohol_g: 0,
-    ash_g: 0,
-    caffeine_mg: 0,
-    theobromine_mg: 0,
-    water_g: 0
-  };
+  nutrition: Nutrition = new Nutrition();
   search = '';
   currentPage = 0;
   currnetLimit = 8;
@@ -171,6 +21,7 @@ export class NutritionComponent implements OnInit {
   constructor(private nutritionService: NutritionService, private toastr: ToastrService) { }
 
   getNutritions() {
+    this.nutrition = new Nutrition();
     if (this.search === '') {
       this.nutritionService.getNutritionsWithPagination(this.currentPage, this.currnetLimit)
       .subscribe(nutritions => {
@@ -194,7 +45,7 @@ export class NutritionComponent implements OnInit {
     this.nutritionService.addNutrition(this.nutrition).subscribe(() => {
       this.toastr.success('You successfully added a nutrition!', 'Success');
       document.getElementById('id01').style.display = 'none'
-      this.clearDialog();
+      //this.clearDialog();
       this.getNutritions();
     }, (err) => {
       this.toastr.error(err.error.message, 'Error');
@@ -229,7 +80,7 @@ export class NutritionComponent implements OnInit {
       });
   }
 
-  selectNutrition(option, nutrition) {
+  selectNutrition(option, nutrition: Nutrition) {
     if (nutrition != null && option != null) {
       this.selectedNutrition = nutrition;
       if (option === 2) {
@@ -243,8 +94,7 @@ export class NutritionComponent implements OnInit {
   }
 
   clearDialog() {
-    this.nutrition.name = '';
-    this.nutrition.calories = 0;
+    this.nutrition = new Nutrition();
   }
 
   nextPage() {
--- src/app/models/nutrition.ts
+++ src/app/models/nutrition.ts
@@ -1,149 +1,37 @@
 export class Nutrition {
   _id?: String;
-  name: String;
-  calories: Number;
-  total_fat_g
-    : Number;
-  saturated_fat_g
-    : Number;
-  cholesterol_mg
-    : Number;
-  sodium_mg
-    : Number;
-  choline_mg
-    : Number;
-  folate_mcg
-    : Number;
-  folic_acid_mcg
-    : Number;
-  niacin_mg
-    : Number;
-  pantothenic_acid_mg
-    : Number;
-  riboflavin_mg
-    : Number;
-  thiamin_mg
-    : Number;
-  vitamin_a_IU
-    : Number;
-  vitamin_a_rae_mcg
-    : Number;
-  carotene_alpha_mcg
-    : Number;
-  carotene_beta_mcg
-    : Number;
-  cryptoxanthin_beta_mcg
-    : Number;
-  lutein_zeaxanthin_mcg
-    : Number;
-  vitamin_b12_mcg
-    : Number;
-  vitamin_b6_mg
-    : Number;
-  vitamin_c_mg
-    : Number;
-  vitamin_d_IU
-    : Number;
-  vitamin_e_mg
-    : Number;
-  tocopherol_alpha_mg
-    : Number;
-  vitamin_k_mcg
-    : Number;
-  calcium_mg
-    : Number;
-  copper_mg
-    : Number;
-  irom_mg
-    : Number;
-  magnesium_mg
-    : Number;
-  manganese_mg
-    : Number;
-  phosphorous_mg
-    : Number;
-  potassium_mg
-    : Number;
-  selenium_mcg
-    : Number;
-  zink_mg
-    : Number;
-  protein_g
-    : Number;
-  alanine_g
-    : Number;
-  arginine_g
-    : Number;
-  aspartic_acid_g
-    : Number;
-  cystine_g
-    : Number;
-  glutamic_acid_g
-    : Number;
-  glycine_g
-    : Number;
-  histidine_g
-    : Number;
-  hydroxyproline_g
-    : Number;
-  isoleucine_g
-    : Number;
-  leucine_g
-    : Number;
-  lysine_g
-    : Number;
-  methionine_g
-    : Number;
-  phenylalanine_g
-    : Number;
-  proline_g
-    : Number;
-  serine_g
-    : Number;
-  threonine_g
-    : Number;
-  tryptophan_g
-    : Number;
-  tyrosine_g
-    : Number;
-  valine_g
-    : Number;
-  carbohydrate_g
-    : Number;
-  fiber_g
-    : Number;
-  sugars_g
-    : Number;
-  fructose_g
-    : Number;
-  galactose_g
-    : Number;
-  glucose_g
-    : Number;
-  lactose_g
-    : Number;
-  maltose_g
-    : Number;
-  sucrose_g
-    : Number;
-  fat_g
-    : Number;
-  saturated_fatty_acids_g
-    : Number;
-  monounsaturated_fatty_acids_g
-    : Number;
-  polyunsaturated_fatty_acids_g
-    : Number;
-  fatty_acids_total_trans_g
-    : Number;
-  alcohol_g
-    : Number;
-  ash_g
-    : Number;
-  caffeine_mg
-    : Number;
-  theobromine_mg
-    : Number;
-  water_g
-    : Number;
+  name: String = "";
+  calories: Number = 0;
+  carbohydrate_g: Number = 0;
+  fiber_g: Number = 0;
+  protein_g: Number = 0;
+  total_fat_g: Number = 0;
+  saturated_fat_g: Number = 0;
+  fatty_acids_total_trans_g: Number = 0;
+  cholesterol_mg: Number = 0;
+  sugars_g: Number = 0;
+  water_g: Number = 0;
+  vitamin_a_rae_mcg: Number = 0;
+  thiamin_mg: Number = 0;
+  riboflavin_mg: Number = 0;
+  niacin_mg: Number = 0;
+  pantothenic_acid_mg: Number = 0;
+  vitamin_b6_mg: Number = 0;
+  folate_mcg: Number = 0;
+  vitamin_b12_mcg: Number = 0;
+  choline_mg: Number = 0;
+  vitamin_c_mg: Number = 0;
+  vitamin_d_IU: Number = 0;
+  vitamin_e_mg: Number = 0;
+  vitamin_k_mcg: Number = 0;
+  calcium_mg: Number = 0;
+  copper_mg: Number = 0;
+  irom_mg: Number = 0;
+  magnesium_mg: Number = 0;
+  manganese_mg: Number = 0;
+  phosphorous_mg: Number = 0;
+  potassium_mg: Number = 0;
+  selenium_mcg: Number = 0;
+  sodium_mg: Number = 0;
+  zink_mg: Number = 0;
 }
