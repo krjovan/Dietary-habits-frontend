52fece312cb04d2aeebd1d12782d6ceab1489237
krjovan <jovankron@gmail.com>
2022-02-20 13:36:40 +0100

    Graphs color based on low, perfect and high intake implemented

--- src/app/components/my-diet/my-diet.component.ts
+++ src/app/components/my-diet/my-diet.component.ts
@@ -15,6 +15,7 @@ import { Dri } from '../../models/dri';
 export class MyDietComponent implements OnInit {
 
   dri: Dri = <Dri>{};
+  colors: any = <any>{};
   isLoaded: Boolean = false;
   dateOfConsumption = new Date();
   nutritions: any[] = [];
@@ -41,6 +42,7 @@ export class MyDietComponent implements OnInit {
       .subscribe(nutritions => {
         this.nutritions = nutritions;
         this.calculateSum(nutritions);
+        this.assignColor();
         this.updateCharts();
         this.isLoaded = true;
         this.toastr.success('Found ' + nutritions.length + ' nutrition/s', 'Success');
@@ -58,6 +60,20 @@ export class MyDietComponent implements OnInit {
       for (let j = 0; j < sumNutritionKeys.length; j++) {
         let key = sumNutritionKeys[j];
         this.sumNutritions[key] += nutrition[key] * nutritionQuantityRatio;
+      }
+    }
+  }
+
+  assignColor() {
+    let sumNutritionKeys = Object.keys(this.sumNutritions);
+    for (let i = 0; i < sumNutritionKeys.length; i++) {
+      let key = sumNutritionKeys[i];
+      if (this.sumNutritions[key] < this.dri[key]) {
+        this.colors[key] = "#2196F3";
+      } else if (this.sumNutritions[key] >= this.dri[key] && this.sumNutritions[key] <= this.dri[key + "_max"]) {
+        this.colors[key] = "#4CAF50";
+      } else if (this.sumNutritions[key] > this.dri[key + "_max"]) {
+        this.colors[key] = "#f44336";
       }
     }
   }
@@ -89,7 +105,7 @@ export class MyDietComponent implements OnInit {
   }
 
   updateCharts() {
-    let chart = new CanvasJS.Chart("chartContainer", {
+    /*let chart = new CanvasJS.Chart("chartContainer", {
       theme: "light2",
       animationEnabled: true,
       exportEnabled: true,
@@ -109,83 +125,7 @@ export class MyDietComponent implements OnInit {
       }]
     });
 
-    chart.render();
-
-    var chart2 = new CanvasJS.Chart("chartContainer2", {
-      animationEnabled: true,
-      theme: "light2",
-      title:{
-        text: "Water intake"
-      },
-      axisY: {
-        title: "Water (g)"
-      },
-      toolTip: {
-        shared: true
-      },
-      data: [{
-        type: "column",
-        name: "Consumed",
-        color: "#0099cc",
-        toolTipContent: "<span style=\"color:#0099cc\">{name}</span>: {y} (g)",
-        dataPoints: [
-            { y: this.sumNutritions.water_g, label: "Today" }
-          ]
-        },
-        {
-          type: "error",
-          name: "Variance",
-          color: "red",
-          toolTipContent: "<span style=\"color:#C0504E\">{name}</span>: {y[0]} - {y[1]} (g)",
-          dataPoints: [
-            { y: [this.dri.water_g, this.dri.water_g_max], label: "Today" }
-          ]
-        }
-      ]
-    });
-    chart2.render();
-
-    var chart1 = new CanvasJS.Chart("chartContainer1", {
-      animationEnabled: true,
-      title: {
-        text: "Vitamins"
-      },
-      axisX: {
-        interval: 1
-      },
-      axisY: {
-        scaleBreaks: {
-          type: "wavy",
-          customBreaks: [{
-            startValue: 150,
-            endValue: 5000
-          }
-          ]
-        }
-      },
-      data: [{
-        type: "bar",
-        toolTipContent: "<b>{label}</b>",
-        dataPoints: [
-          { label: "Vitamin C", y: this.sumNutritions.vitamin_c_mg / this.dri.vitamin_c_mg * 100 },
-          { label: "Vitamin B3", y: this.sumNutritions.niacin_mg / this.dri.niacin_mg * 100 },
-          { label: "Vitamin B2", y: this.sumNutritions.riboflavin_mg / this.dri.riboflavin_mg * 100 }
-        ]
-      },
-      {
-        type: "error",
-        name: "Variability Range",
-        toolTipContent: "<span style=\"color:#C0504E\">{name}</span>: {y[0]} - {y[1]}",
-        dataPoints: [
-          { y: [100, this.dri.vitamin_c_mg_max / this.dri.vitamin_c_mg * 100], label: "Vitamin C" },
-          { y: [100, this.dri.niacin_mg_max / this.dri.niacin_mg * 100], label: "Vitamin B3" },
-          { y: [100, this.dri.riboflavin_mg_max / this.dri.riboflavin_mg * 100], label: "Vitamin B2" }
-        ]
-      }
-      ]
-    });
-    chart1.render();
-
+    chart.render();*/
 
     var macronutrients = new CanvasJS.Chart("macronutrients", {
       animationEnabled: true,
@@ -209,21 +149,22 @@ export class MyDietComponent implements OnInit {
         type: "bar",
         toolTipContent: "<span style=\"color:#0099cc\">{label}</span>: {y} (g)",
         dataPoints: [
-          { label: "Water", y: this.sumNutritions.water_g / this.dri.water_g * 100 },
-          { label: "Sugar", y: this.sumNutritions.sugars_g / this.dri.sugars_g * 100 },
-          { label: "Cholesterol", y: this.sumNutritions.cholesterol_mg / this.dri.cholesterol_mg * 100 },
-          { label: "Total trans fat", y: this.sumNutritions.fatty_acids_total_trans_g / this.dri.fatty_acids_total_trans_g * 100 },
-          { label: "Saturated fat", y: this.sumNutritions.saturated_fat_g / this.dri.saturated_fat_g * 100 },
-          { label: "Total fat", y: this.sumNutritions.total_fat_g / this.dri.total_fat_g * 100 },
-          { label: "Proteins", y: this.sumNutritions.protein_g / this.dri.protein_g * 100 },
-          { label: "Fiber", y: this.sumNutritions.fiber_g / this.dri.fiber_g * 100 },
-          { label: "Carbohydrates", y: this.sumNutritions.carbohydrate_g / this.dri.carbohydrate_g * 100 },
-          { label: "Calories", y: this.sumNutritions.calories / this.dri.calories * 100 }
+          { label: "Water", y: this.sumNutritions.water_g / this.dri.water_g * 100, color:this.colors["water_g"] },
+          { label: "Sugar", y: this.sumNutritions.sugars_g / this.dri.sugars_g * 100, color:this.colors["sugars_g"] },
+          { label: "Cholesterol", y: this.sumNutritions.cholesterol_mg / this.dri.cholesterol_mg * 100, color:this.colors["cholesterol_mg"] },
+          { label: "Total trans fat", y: this.sumNutritions.fatty_acids_total_trans_g / this.dri.fatty_acids_total_trans_g * 100, color:this.colors["fatty_acids_total_trans_g"] },
+          { label: "Saturated fat", y: this.sumNutritions.saturated_fat_g / this.dri.saturated_fat_g * 100, color:this.colors["saturated_fat_g"] },
+          { label: "Total fat", y: this.sumNutritions.total_fat_g / this.dri.total_fat_g * 100, color:this.colors["total_fat_g"] },
+          { label: "Proteins", y: this.sumNutritions.protein_g / this.dri.protein_g * 100, color:this.colors["protein_g"] },
+          { label: "Fiber", y: this.sumNutritions.fiber_g / this.dri.fiber_g * 100, color:this.colors["fiber_g"] },
+          { label: "Carbohydrates", y: this.sumNutritions.carbohydrate_g / this.dri.carbohydrate_g * 100, color:this.colors["carbohydrate_g"] },
+          { label: "Calories", y: this.sumNutritions.calories / this.dri.calories * 100, color:this.colors["calories"] }
         ]
       },
       {
         type: "error",
         name: "Healthy range",
+        color: "black",
         toolTipContent: "<span style=\"color:#C0504E\">{name}</span>: {y[0]} - {y[1]}",
         dataPoints: [
           { y: [100, this.dri.water_g_max / this.dri.water_g * 100], label: "Water" },
@@ -241,6 +182,124 @@ export class MyDietComponent implements OnInit {
       ]
     });
     macronutrients.render();
+
+    var vitamins = new CanvasJS.Chart("vitamins", {
+      animationEnabled: true,
+      zoomEnabled: true,
+      zoomType: "y",
+      title: {
+        text: "Vitamins"
+      },
+      axisX: {
+        interval: 1
+      },
+      axisY: {
+        viewportMinimum: 0,
+        viewportMaximum: 150,
+        valueFormatString:  "#' %'"
+      },
+      toolTip: {
+        shared: true
+      },
+      data: [{
+        type: "bar",
+        toolTipContent: "<b>{label}</b>",
+        dataPoints: [
+          { label: "vitamin_k_mcg", y: this.sumNutritions.vitamin_k_mcg / this.dri.vitamin_k_mcg * 100, color:this.colors["vitamin_k_mcg"] },
+          { label: "vitamin_e_mg", y: this.sumNutritions.vitamin_e_mg / this.dri.vitamin_e_mg * 100, color:this.colors["vitamin_e_mg"] },
+          { label: "vitamin_d_IU", y: this.sumNutritions.vitamin_d_IU / this.dri.vitamin_d_IU * 100, color:this.colors["vitamin_d_IU"] },
+          { label: "vitamin_c_mg", y: this.sumNutritions.vitamin_c_mg / this.dri.vitamin_c_mg * 100, color:this.colors["vitamin_c_mg"] },
+          { label: "choline_mg", y: this.sumNutritions.choline_mg / this.dri.choline_mg * 100, color:this.colors["choline_mg"] },
+          { label: "vitamin_b12_mcg", y: this.sumNutritions.vitamin_b12_mcg / this.dri.vitamin_b12_mcg * 100, color:this.colors["vitamin_b12_mcg"] },
+          { label: "folate_mcg", y: this.sumNutritions.folate_mcg / this.dri.folate_mcg * 100, color:this.colors["folate_mcg"] },
+          { label: "vitamin_b6_mg", y: this.sumNutritions.vitamin_b6_mg / this.dri.vitamin_b6_mg * 100, color:this.colors["vitamin_b6_mg"] },
+          { label: "pantothenic_acid_mg", y: this.sumNutritions.pantothenic_acid_mg / this.dri.pantothenic_acid_mg * 100, color:this.colors["pantothenic_acid_mg"] },
+          { label: "niacin_mg", y: this.sumNutritions.niacin_mg / this.dri.niacin_mg * 100, color:this.colors["niacin_mg"] },
+          { label: "riboflavin_mg", y: this.sumNutritions.riboflavin_mg / this.dri.riboflavin_mg * 100, color:this.colors["riboflavin_mg"] },
+          { label: "thiamin_mg", y: this.sumNutritions.thiamin_mg / this.dri.thiamin_mg * 100, color:this.colors["thiamin_mg"] },
+          { label: "vitamin_a_rae_mcg", y: this.sumNutritions.vitamin_a_rae_mcg / this.dri.vitamin_a_rae_mcg * 100, color:this.colors["vitamin_a_rae_mcg"] }
+        ]
+      },
+      {
+        type: "error",
+        name: "Healthy range",
+        color: "black",
+        toolTipContent: "<span style=\"color:#C0504E\">{name}</span>: {y[0]} - {y[1]}",
+        dataPoints: [
+          { y: [100, this.dri.vitamin_k_mcg_max / this.dri.vitamin_k_mcg * 100], label: "vitamin_k_mcg" },
+          { y: [100, this.dri.vitamin_e_mg_max / this.dri.vitamin_e_mg * 100], label: "vitamin_e_mg" },
+          { y: [100, this.dri.vitamin_d_IU_max / this.dri.vitamin_d_IU * 100], label: "vitamin_d_IU" },
+          { y: [100, this.dri.vitamin_c_mg_max / this.dri.vitamin_c_mg * 100], label: "vitamin_c_mg" },
+          { y: [100, this.dri.choline_mg_max / this.dri.choline_mg * 100], label: "choline_mg" },
+          { y: [100, this.dri.vitamin_b12_mcg_max / this.dri.vitamin_b12_mcg * 100], label: "vitamin_b12_mcg" },
+          { y: [100, this.dri.folate_mcg_max / this.dri.folate_mcg * 100], label: "folate_mcg" },
+          { y: [100, this.dri.vitamin_b6_mg_max / this.dri.vitamin_b6_mg * 100], label: "vitamin_b6_mg" },
+          { y: [100, this.dri.pantothenic_acid_mg_max / this.dri.pantothenic_acid_mg * 100], label: "pantothenic_acid_mg" },
+          { y: [100, this.dri.niacin_mg_max / this.dri.niacin_mg * 100], label: "niacin_mg" },
+          { y: [100, this.dri.riboflavin_mg_max / this.dri.riboflavin_mg * 100], label: "riboflavin_mg" },
+          { y: [100, this.dri.thiamin_mg_max / this.dri.thiamin_mg * 100], label: "thiamin_mg" },
+          { y: [100, this.dri.vitamin_a_rae_mcg_max / this.dri.vitamin_a_rae_mcg * 100], label: "vitamin_a_rae_mcg" }
+        ]
+      }
+      ]
+    });
+    vitamins.render();
+
+    var minerals = new CanvasJS.Chart("minerals", {
+      animationEnabled: true,
+      zoomEnabled: true,
+      zoomType: "y",
+      title: {
+        text: "Minerals"
+      },
+      axisX: {
+        interval: 1
+      },
+      axisY: {
+        viewportMinimum: 0,
+        viewportMaximum: 150,
+        valueFormatString:  "#' %'"
+      },
+      toolTip: {
+        shared: true
+      },
+      data: [{
+        type: "bar",
+        toolTipContent: "<span style=\"color:#0099cc\">{label}</span>: {y} (g)",
+        dataPoints: [
+          { label: "zink_mg", y: this.sumNutritions.zink_mg / this.dri.zink_mg * 100, color:this.colors["zink_mg"] },
+          { label: "sodium_mg", y: this.sumNutritions.sodium_mg / this.dri.sodium_mg * 100, color:this.colors["sodium_mg"] },
+          { label: "selenium_mcg", y: this.sumNutritions.selenium_mcg / this.dri.selenium_mcg * 100, color:this.colors["selenium_mcg"] },
+          { label: "potassium_mg", y: this.sumNutritions.potassium_mg / this.dri.potassium_mg * 100, color:this.colors["potassium_mg"] },
+          { label: "phosphorous_mg", y: this.sumNutritions.phosphorous_mg / this.dri.phosphorous_mg * 100, color:this.colors["phosphorous_mg"] },
+          { label: "manganese_mg", y: this.sumNutritions.manganese_mg / this.dri.manganese_mg * 100, color:this.colors["manganese_mg"] },
+          { label: "magnesium_mg", y: this.sumNutritions.magnesium_mg / this.dri.magnesium_mg * 100, color:this.colors["magnesium_mg"] },
+          { label: "irom_mg", y: this.sumNutritions.irom_mg / this.dri.irom_mg * 100, color:this.colors["irom_mg"] },
+          { label: "copper_mg", y: this.sumNutritions.copper_mg / this.dri.copper_mg * 100, color:this.colors["copper_mg"] },
+          { label: "calcium_mg", y: this.sumNutritions.calcium_mg / this.dri.calcium_mg * 100, color:this.colors["calcium_mg"] }
+        ]
+      },
+      {
+        type: "error",
+        name: "Healthy range",
+        color: "black",
+        toolTipContent: "<span style=\"color:#C0504E\">{name}</span>: {y[0]} - {y[1]}",
+        dataPoints: [
+          { y: [100, this.dri.zink_mg_max / this.dri.zink_mg * 100], label: "zink_mg" },
+          { y: [100, this.dri.sodium_mg_max / this.dri.sodium_mg * 100], label: "sodium_mg" },
+          { y: [100, this.dri.selenium_mcg_max / this.dri.selenium_mcg * 100], label: "selenium_mcg" },
+          { y: [100, this.dri.potassium_mg_max / this.dri.potassium_mg * 100], label: "potassium_mg" },
+          { y: [100, this.dri.phosphorous_mg_max / this.dri.phosphorous_mg * 100], label: "phosphorous_mg" },
+          { y: [100, this.dri.manganese_mg_max / this.dri.manganese_mg * 100], label: "manganese_mg" },
+          { y: [100, this.dri.magnesium_mg_max / this.dri.magnesium_mg * 100], label: "magnesium_mg" },
+          { y: [100, this.dri.irom_mg_max / this.dri.irom_mg * 100], label: "irom_mg" },
+          { y: [100, this.dri.copper_mg_max / this.dri.copper_mg * 100], label: "copper_mg" },
+          { y: [100, this.dri.calcium_mg_max / this.dri.calcium_mg * 100], label: "calcium_mg" }
+        ]
+      }
+      ]
+    });
+    minerals.render();
   }
 
   ngOnInit(): void {
