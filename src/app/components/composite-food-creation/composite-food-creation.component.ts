c83f15b6082f92cfdb91924ad8cf18cc2f68a718
krjovan <jovankron@gmail.com>
2022-04-09 17:04:42 +0200

    Added array of ingredients and a responsive table to display the array

--- src/app/components/composite-food-creation/composite-food-creation.component.ts
+++ src/app/components/composite-food-creation/composite-food-creation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { Nutrition } from 'src/app/models/nutrition';
 import { NutritionService } from 'src/app/services/nutrition.service';
 
 @Component({
@@ -12,13 +13,14 @@ export class CompositeFoodCreationComponent implements OnInit {
   search = '';
   nutritions: any [] = [];
   isLoaded: Boolean = true;
+  ingredients: any[] = [];
 
   constructor(private nutritionService: NutritionService,
     private toastr: ToastrService) { }
 
   getNutritions() {
     this.isLoaded = false;
-      this.nutritionService.getNutritionNamesAndIds(this.search)
+      this.nutritionService.getNutritions(this.search)
       .subscribe(nutritions => {
         this.nutritions = nutritions;
         this.isLoaded = true;
@@ -26,17 +28,24 @@ export class CompositeFoodCreationComponent implements OnInit {
     });
   }
 
-  addIngredient(id) {
-    const x = document.getElementById(id.toString()) as HTMLInputElement;
-    if(Number(x.value) === 0) {
+  addIngredient(nutrition) {
+    const qunatity = document.getElementById(nutrition._id.toString()) as HTMLInputElement;
+    if(Number(qunatity.value) === 0) {
       this.toastr.error('You must enter a proper value for quantity.', 'Error');
     } else {
-      console.log(id, Number(x.value));
+      this.ingredients.push({nutrition: nutrition, ingredient_quantity: qunatity.value});
+      console.table(this.ingredients);
     }
   }
 
   openAddForm() {
     document.getElementById('id01').style.display = 'block';
+  }
+
+  removeIngredient(id) {
+    let indexOfIngredientToBeRemoved = this.ingredients.map((element) => element.nutrition._id).indexOf(id);
+    this.ingredients.splice(indexOfIngredientToBeRemoved, 1);
+    console.table(this.ingredients);
   }
 
   ngOnInit(): void {
